6e5014731f5ce8a361dcd8c82ee0672e98057576
bryanalberd <bryanplara568@gmail.com>
2024-09-26 14:22:25 +0700

    Add files via upload

---
+++ Tugas.py
@@ -0,0 +1,15 @@
+# buat perhitungan matematika mengenai bangun ruang 2D atau 3D
+#volume bola
+# phi = 3.14
+# r  = float(input("masukkan nilai jari-jari : "))
+# v = 4/3 * phi * r * r * r
+
+# print("volume bola adalah : ", v)
+
+#volume kerucut
+π = 3.14
+r = float(input("masukkan panjang jari-jari kerucut : "))
+t = float(input("masukkan tinggi kerucut : "))
+v = 1/3 * π * r * r * t
+
+print("volume kerucut adalah : ", v)
